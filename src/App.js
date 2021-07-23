5de49b9d4a97964b36de446d4f1938fedd916b1e
Franck_Tscherig <fjtsystemsg@gmail.com>
2021-07-22 23:52:19 -0300

    Se acomodo carpetas y se modifico boto para activar COMPONENTElo visto en la clase n6

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from 'react'
+import logo from './logo.svg';
+import './App.css';
 
-import {useState,useEffect} from 'react';
-
-const App = () => {
-  const [name, setName] = useState(null);
-
-  useEffect(() => {
-    fetch('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=344d40df0c8cc373141c1dc321fae9cf&hash=bd0722d5750b6362d5ba0212ca36726b') // hacemos la petición get
-    .then(res => res.json()) // cuando hayamos terminado (then) e texto plano lo parseamos a json la respuesta de la petición
-    .then(res => setName(res.data.results[10].modified)); // cuando hayamos terminado (then) actualizamos el estado nombre
-  }, []); //Debemos usar los [] para que la petición sólo se ejecute cuando el componente se monte. De lo contrario se ejecutaría en cada render. Si ponemos una variable de estado dentro de los [], la petición se ejecutárá cada vez que esa variable cambie.
-  return <p>modified:{name}</p>;
-
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
-
-
